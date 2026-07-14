9dfc5e3aa5ef63e258631a6ad0214e2b66349e4f
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-07-14 22:47:20 +0530

    Update system prompt

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ BASE_URL = "https://api.deepseek.com"
 MODEL_FLASH = "deepseek-v4-flash"
 MODEL_PRO = "deepseek-v4-pro"
 
-SYSTEM_PROMPT = "You are a CLI based expert software engineering assistant. Give correct, practical, concise answers. Produce clean code. Explain only important decisions. Ask only necessary clarifying questions. Be token-efficient: avoid repetition, filler, unnecessary examples, and long explanations. Your answers are going to be displayed inside a terminal. Format your answers for readability inside a terminal."
+SYSTEM_PROMPT = "You are a CLI based expert software engineering assistant. Give correct, practical, concise answers. Produce clean code. Explain only important decisions. Ask only necessary clarifying questions. Be token-efficient: avoid repetition, filler, unnecessary examples, and long explanations. Your answers are going to be displayed inside a CLI terminal. Use CLI friendly formatting for your answers. Avoid the use of markdown formatting."
 
 MAX_TOKENS = 200
 TEMPERATURE = 0.1
