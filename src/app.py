dd3872e6cc87909fec810dbc6fc3365441f5734c
Sarang Dave <131434471+S84v@users.noreply.github.com>
2026-07-14 20:21:31 +0530

    Add chat loop

--- src/app.py
+++ src/app.py
@@ -8,22 +8,39 @@ load_dotenv()
 api_key = os.getenv("DEEPSEEK_API_KEY")
 
 if api_key is None:
-    print("DeepSeek API key not found.")
+    exit("DeepSeek API key not found.")
 
 client = OpenAI(api_key=api_key, base_url=config.BASE_URL)
 
-response = client.chat.completions.create(
-    model=config.MODEL_FLASH,
-    messages=[
-        {"role": "system", "content": config.SYSTEM_PROMPT},
-        {"role": "user", "content": "hello"},
-    ],
-    max_tokens=config.MAX_TOKENS,
-    temperature=config.TEMPERATURE,
-    stream=True,
-    extra_body={"thinking": {"type": "disabled"}},
-)
+messages = [
+    {"role": "system", "content": config.SYSTEM_PROMPT},
+]
 
-for chunk in response:
-    delta = chunk.choices[0].delta.content or ""
-    print(delta, end = "", flush = True)
+print("Hello. Welcome to CLI Programming Assistant. Write your query below:")
+
+while True:
+    assistant_response = ""
+    user_input = input("\n> ")
+
+    if user_input == "exit":
+        break
+
+    messages.append({"role": "user", "content": user_input})
+
+    response = client.chat.completions.create(
+        model=config.MODEL_FLASH,
+        messages=messages,
+        max_tokens=config.MAX_TOKENS,
+        temperature=config.TEMPERATURE,
+        stream=True,
+        extra_body={"thinking": {"type": "disabled"}},
+    )
+
+    for chunk in response:
+        delta = chunk.choices[0].delta.content or ""
+        assistant_response += delta
+        print(delta, end="", flush=True)
+
+    messages.append({"role": "assistant", "content": assistant_response})
+
+    print(messages)
